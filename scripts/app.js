90086eac17a827151afb5673fa4bce9fe988a914
inadram <inadram@gmail.com>
2016-05-13 22:39:29 +0100

    remove unused constants

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name editogramApp
- * @description
- * # urbanApp
- *
- * Main module of the application.
- */
 angular
     .module('editogramApp', [
         'ui.router',
@@ -24,17 +16,4 @@ angular
     .constant('scope', ['email', 'profile', 'https://www.googleapis.com/auth/drive'])
     .constant('loadApis', {
         'drive' : 'v2'
-    })
-    .constant('COLORS', {
-        'default': '#e2e2e2',
-        primary: '#09c',
-        success: '#2ECC71',
-        warning: '#ffc65d',
-        danger: '#d96557',
-        info: '#4cc3d9',
-        white: 'white',
-        dark: '#4C5064',
-        border: '#e4e4e4',
-        bodyBg: '#e0e8f2',
-        textColor: '#6B6B6B',
     });
