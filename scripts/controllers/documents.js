1f1272486a9d0f9327bf3a495a799eebdb6d60e9
inadram <inadram@gmail.com>
2016-06-01 21:45:14 +0100

    load more documents

--- scripts/controllers/documents.js
+++ scripts/controllers/documents.js
@@ -6,16 +6,19 @@ function documentsController(googleDocument, $scope) {
 
     function getAllDocuments(continuationToken) {
         googleDocument.getAll(continuationToken).then(function (response) {
-            $scope.documents = response.response.result;
+            $scope.documents = $scope.documents || [];
+            var documents = response.response.result;
+            $scope.lastToken = documents[documents.length-1].continuationToken;
+            $scope.documents.push.apply($scope.documents, documents);
+            
         });
     }
 
-
-    $scope.loadMore = function (continuationToken) {
-      $scope.push(getAllDocuments(continuationToken))
+    $scope.loadMore = function () {
+        $scope.push(getAllDocuments($scope.lastToken))
     }
 }
 
 angular
     .module('editogramApp')
-    .controller('documentsController', ['googleDocument','$scope', documentsController]);
+    .controller('documentsController', ['googleDocument', '$scope', documentsController]);
